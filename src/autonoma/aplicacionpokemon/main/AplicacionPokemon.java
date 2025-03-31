9a22651acda746192f1fb3a7edd45ae159c68f83
Kallst <santiago.castrom@autonoma.edu.co>
2025-03-31 16:50:04 -0500

    Se creo la clase main, junto con los commit y los archivos generados automáticamente por java NetBeans

---
+++ src/autonoma/aplicacionpokemon/main/AplicacionPokemon.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package autonoma.aplicacionpokemon.main;
+
+/**
+ *
+ * @author LAPTOP
+ */
+public class AplicacionPokemon {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
