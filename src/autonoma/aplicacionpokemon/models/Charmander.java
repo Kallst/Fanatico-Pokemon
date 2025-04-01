25eac4a06231b8f1ae29e80811f037dabbfd0c79
Kallst <santiago.castrom@autonoma.edu.co>
2025-03-31 19:28:54 -0500

    Se configuro la clase Charmander

--- src/autonoma/aplicacionpokemon/models/Charmander.java
+++ src/autonoma/aplicacionpokemon/models/Charmander.java
@@ -1,13 +1,42 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package autonoma.aplicacionpokemon.models;
+
+import autonoma.aplicacionpokemon.views.IFuego;
 
 /**
  *
- * @author LAPTOP
+ * @author Santiago Castro Marles
  */
-public class Charmander {
+
+public class Charmander extends Pokemon implements IFuego{
+
+    @Override
+    public void atacarPlacaje() {
+        System.out.println("Soy Charmander y voy a atacar con placaje");
+    }
+
+    @Override
+    public void atacarAraniazo() {
+        System.out.println("Soy Charmander y voy a atacar con araniazo");
+    }
+
+    @Override
+    public void atacarMordizco() {
+        System.out.println("Soy Charmander y voy a atacar con mordizco");
+    }
+
+    @Override
+    public void atacarPunioFuego() {
+        System.out.println("Soy Charmander y voy a atacar con punio ");
+    }
+
+    @Override
+    public void atacarAscuas() {
+        System.out.println("Soy Charmander y voy a atacar con ascuas");
+    }
+
+    @Override
+    public void atacarLanzallamas() {
+        System.out.println("Soy Charmander y voy a atacar con lanza llamas");
+    }
     
 }
