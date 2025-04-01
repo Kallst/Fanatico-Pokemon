8b415ec937df515a171c6fdd008d41e22984fc13
Kallst <santiago.castrom@autonoma.edu.co>
2025-03-31 19:26:05 -0500

    Se configuro la clase Bulbasur

--- src/autonoma/aplicacionpokemon/models/Bulbasur.java
+++ src/autonoma/aplicacionpokemon/models/Bulbasur.java
@@ -1,13 +1,46 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package autonoma.aplicacionpokemon.models;
+
+import autonoma.aplicacionpokemon.views.IPlanta;
 
 /**
  *
- * @author LAPTOP
+ * @author Santiago Castro Marles
  */
-public class Bulbasur {
+public class Bulbasur extends Pokemon implements IPlanta {
+
+    @Override
+    public void atacarPlacaje() {
+        System.out.println("Soy Bulbasur y voy a atacar con placaje");
+    }
+
+    @Override
+    public void atacarAraniazo() {
+        System.out.println("Soy Bulbasur y voy a atacar con araniazo");
+    }
+
+    @Override
+    public void atacarMordizco() {
+        System.out.println("Soy Bulbasur y voy a atacar con mordisco");
+    }
+
+    @Override
+    public void atacarParalizar() {
+        System.out.println("Soy Bulbasur y voy a atacar con paralizar");
+    }
+
+    @Override
+    public void atacarDrenaje() {
+        System.out.println("Soy Bulbasur y voy a atacar con drenaje");
+    }
+
+    @Override
+    public void atacarHojaAfilada() {
+        System.out.println("Soy Bulbasur y voy a atacar con hoja afilada");
+    }
+
+    @Override
+    public void atacarLatigoCepa() {
+        System.out.println("Soy Bulbasur y voy a atacar con latigo cepa");
+    }
     
 }
