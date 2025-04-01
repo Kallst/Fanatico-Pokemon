05541b7e7abfed95093377c9e34f5b1ef7536902
Kallst <santiago.castrom@autonoma.edu.co>
2025-03-31 19:22:34 -0500

    Se configuró la clase Pikachu

--- src/autonoma/aplicacionpokemon/models/Pikachu.java
+++ src/autonoma/aplicacionpokemon/models/Pikachu.java
@@ -1,13 +1,47 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package autonoma.aplicacionpokemon.models;
+
+import autonoma.aplicacionpokemon.views.IElectrico;
 
 /**
  *
- * @author LAPTOP
+ * @author Santiago Castro Marles 
  */
-public class Pikachu {
+
+public class Pikachu extends Pokemon implements IElectrico {
+
+    @Override
+    public void atacarPlacaje() {
+        System.out.println("Soy Pikachu y estoy atacando con placaje");
+    }
+
+    @Override
+    public void atacarAraniazo() {
+        System.out.println("Soy Pikachu y estoy atacando con araniazo");
+    }
+
+    @Override
+    public void atacarMordizco() {
+        System.out.println("Soy Pikachu y estoy atacando con mordisco");
+    }
+
+    @Override
+    public void atacarImpactrueno() {
+        System.out.println("Soy Pikachu y estoy atacando con impactrueno");
+    }
+
+    @Override
+    public void atacarPunioTrueno() {
+        System.out.println("Soy Pikachu y estoy atacando con punio trueno");
+    }
+
+    @Override
+    public void atacarRayo() {
+        System.out.println("Soy Pikachu y estoy atacando con rayo");
+    }
+
+    @Override
+    public void atacarRayoCarga() {
+        System.out.println("Soy Pikachu y estoy atacando con rayo carga");
+    }
     
 }
